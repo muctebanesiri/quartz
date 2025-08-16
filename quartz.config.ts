525e42a808c5a301d93baca01b9554e251f4550f
ㅤ <muctebanesiri@gmail.com>
2025-08-16 05:40:27 +0330

    bg color

--- quartz.config.ts
+++ quartz.config.ts
@@ -40,7 +40,7 @@ const config: QuartzConfig = {
           textHighlight: "#fff23688",
         },
         darkMode: {
-          light: "#161618",
+          light: "#100f0f",
           lightgray: "#393639",
           gray: "#646464",
           darkgray: "#d4d4d4",
