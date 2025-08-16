614351bf501ea8ba4d1ea9d46e27c766501d592c
ㅤ <muctebanesiri@gmail.com>
2025-08-16 10:24:31 +0330

    Update Footer.tsx

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -15,7 +15,7 @@ export default ((opts?: Options) => {
       <footer class={`${displayClass ?? ""}`}>
         <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz</a> with <a href="https://obsidian.md/">Obsidian</a> © {year}
+          <a href="https://quartz.jzhao.xyz/">Quartz</a> and <a href="https://obsidian.md/">Obsidian</a> © {year}
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
