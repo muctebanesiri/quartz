7f04e91a707e2f52086c08c8f583ca32b59d311a
ㅤ <muctebanesiri@gmail.com>
2025-08-17 05:44:47 +0330

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,8 +23,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Noto Naskh Arabic",
-        body: "Noto Naskh Arabic",
+        header: "vazirmatn",
+        body: "vazirmatn",
         code: "IBM Plex Mono",
       },
       colors: {
