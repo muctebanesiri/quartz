e3941e0bef4d094daece931a124d4be4a64f0446
ㅤ <muctebanesiri@gmail.com>
2025-08-21 01:00:00 +0330

    Revert "Update Footer.tsx"
    This reverts commit 637ac4b6a4d8af81b0338cd8857d41e829218e94.

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -13,6 +13,10 @@ export default ((opts?: Options) => {
     const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
+        <p>
+          {i18n(cfg.locale).components.footer.createdWith}{" "}
+            CC-BY-NC
+        </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
             <li>
