1a80360fdef39aacf52990a0436a4d51bb353564
ㅤ <muctebanesiri@gmail.com>
2025-08-22 01:17:10 +0330

    Update en-US.ts

--- quartz/i18n/locales/en-US.ts
+++ quartz/i18n/locales/en-US.ts
@@ -36,7 +36,7 @@ export default {
       title: "Explorer",
     },
     footer: {
-      createdWith: "Created with",
+      createdWith: "CC-BY unless otherwise noted.",
     },
     graph: {
       title: "Graph View",
